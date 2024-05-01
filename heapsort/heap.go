46e3ddca4235824df4d3ad4173e231f83a6fbb6c
Atsuhiro <atcheri@gmail.com>
2024-05-01 21:32:10 +0200

    feat: add heapify up logic

--- heapsort/heap.go
+++ heapsort/heap.go
@@ -15,10 +15,43 @@ func (h MaxHeap) Size() int {
 }
 
 func (h *MaxHeap) Insert(key int) *MaxHeap {
-	h.slice = append(h.slice, key)
+	newSlice := append(h.slice, key)
+	h.slice = heapify(newSlice, len(newSlice)-1)
+
 	return h
 }
 
 func (h MaxHeap) ToSlice() []int {
 	return h.slice
+}
+
+func heapify(slice []int, from int) []int {
+	for slice[parent(from)] < slice[from] {
+		slice = swap(slice, parent(from), from)
+		from = parent(from)
+	}
+
+	return slice
+}
+
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+func left(i int) int {
+	return 2*i + 1
+
+}
+
+func right(i int) int {
+	return left(i) + 1
+}
+
+func swap(src []int, i, j int) []int {
+	tmp := make([]int, len(src))
+	copy(tmp, src)
+
+	tmp[i], tmp[j] = tmp[j], tmp[i]
+
+	return tmp
 }
