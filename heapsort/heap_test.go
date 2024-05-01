3a608dec9805baf6a70e5763673de64d0cd9b789
Atsuhiro <atcheri@gmail.com>
2024-05-01 21:49:13 +0200

    test: add 2 cases with more items to insert

--- heapsort/heap_test.go
+++ heapsort/heap_test.go
@@ -34,6 +34,21 @@ func Test_Insert(t *testing.T) {
 			input:  []int{5, 6},
 			output: []int{6, 5},
 		},
+		{
+			name:   "inserting 5, 6 4, and 8, returns 8 6 5 4",
+			input:  []int{5, 6, 4, 8},
+			output: []int{8, 6, 4, 5},
+		},
+		{
+			name:   "inserting 10, 5, 3, 2, 4 and 15, returns 15 5 10 2 4 3",
+			input:  []int{10, 5, 3, 2, 4, 15},
+			output: []int{15, 5, 10, 2, 4, 3},
+		},
+		{
+			name:   "inserting 1, 4, 2, 5, 13, 6, 17, returns 17, 5, 13, 5, 1, 4, 2, 6",
+			input:  []int{1, 4, 2, 5, 13, 6, 17},
+			output: []int{17, 5, 13, 1, 4, 2, 6},
+		},
 	}
 
 	for _, tc := range cases {
